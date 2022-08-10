e0b059a3be396b35244486661f8e2abe123db248
Linka Sofia <62571532+LinkaSofia@users.noreply.github.com>
2022-08-09 21:29:14 -0300

    Deletando Diretorio

--- controller/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('DBNAME', 'projetopgp');
-define('PORT', 3306);
-
-$conn = new PDO('mysql:host=' . HOST . ';port='.PORT.';dbname=' . DBNAME . ';', USER, PASS);

e7fe6ee6041009dab3d438788da6ea2f75a22cb8
Linka Sofia <62571532+LinkaSofia@users.noreply.github.com>
2022-08-12 20:59:48 -0300

    Add files via upload

--- controller/ProcessaMercado.php
+++ controller/ProcessaMercado.php
@@ -4,13 +4,19 @@ include_once("conexao1.php");
 
 $Produto = filter_input(INPUT_POST, 'Produto',FILTER_SANITIZE_STRING);
 
-$result_func = "INSERT INTO mercado(Produto) VALUES ('$Produto')";
+if ($Produto <> null){
+	$result_func = "INSERT INTO mercado(Produto) VALUES ('$Produto')";
 
-if ($conn->query($result_func) == TRUE) {
-	echo "Produto Cadastrado";
-	header("Location: compras.php");
-} else {
-	echo "Erro: " . $result_func . "<br>" . $conn->error;
+	if ($conn->query($result_func) == TRUE) {
+		echo "Produto Cadastrado";
+		header("Location: compras.php");
+	} else {
+		echo "Erro: " . $result_func . "<br>" . $conn->error;
+	}
+}else {
+	echo "Digite um valor válido";
 }
+
+
 $conn->close();
 ?>
